e54bdb0323b5cb85a40f339246fe7f613c0b8f2a
Shruti Gupta <shrutigupta5555@gmail.com>
2024-05-02 18:43:42 +0530

    landing page ui

--- src/screens/landing.jsx
+++ src/screens/landing.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import '../css/landing.css'
+import card1 from '../assets/card-1.svg'
+import card2 from '../assets/card-2.svg'
 
 const Landing = () => {
   return (
-    <div>landing</div>
+    <main>
+      <div className="main-container">
+      <div className='left'>
+        <h1>Rehearse AI</h1>
+        <p>practice your interviews, powered by Artificial Intelligence.</p>
+        <div className="buttons-container">
+          <button data="Try Now!"></button>
+          <button data="Learn more"></button>
+        </div>
+      </div>
+      <div className='right'>
+        <img src={card1} alt="" /><br />
+        <img src={card2} alt="" />
+      </div>
+      </div>
+    </main>
   )
 }
 
